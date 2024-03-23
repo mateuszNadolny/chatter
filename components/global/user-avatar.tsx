a72e524c10e1ab26c6c6aa1fe49a50d2d2919be7
mateuszNadolny <mateusznadolny00@gmail.com>
2024-03-23 14:44:57 +0100

    style: fix styling of user avatar for mobile devices

--- components/global/user-avatar.tsx
+++ components/global/user-avatar.tsx
@@ -9,6 +9,13 @@ import {
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog';
+import { Button } from '../ui/button';
+import { ThemeToggle } from '../global/theme-toggle';
+
+import { RiLogoutBoxLine } from 'react-icons/ri';
+
+import { signOut } from 'next-auth/react';
+
 import { getInitials } from '@/lib/helpers';
 import { User } from '@prisma/client';
 
@@ -31,10 +38,21 @@ const UserAvatar = ({ currentUser, className }: UserAvatarProps) => {
       <DialogContent className="rounded-md w-4/5 lg:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>{currentUser.name}</DialogTitle>
+
           <DialogDescription></DialogDescription>
         </DialogHeader>
 
-        <DialogFooter></DialogFooter>
+        <DialogFooter className="w-full flex-row gap-4">
+          <ThemeToggle className="flex lg:hidden w-1/2" />
+          <Button
+            variant="secondary"
+            size="icon"
+            onClick={() => signOut({ callbackUrl: '/' })}
+            className="flex lg:hidden w-1/2">
+            {/* <RiLogoutBoxLine className="h-4 w-4" /> */}
+            Sign out
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
--- components/sidebar/footer.tsx
+++ components/sidebar/footer.tsx
@@ -4,7 +4,6 @@ import useRoutes from '@/hooks/useRoutes';
 
 import UserAvatar from '../global/user-avatar';
 import FooterItem from './footer-item';
-import { ThemeToggle } from '../global/theme-toggle';
 
 import { User } from '@prisma/client';
 
@@ -17,7 +16,7 @@ const Footer = ({ currentUser }: FooterProps) => {
   return (
     <div className="fixed bottom-0 lg:hidden w-full border-t-2">
       <ul className="p-3 flex items-center justify-between w-full h-full">
-        {routes.map((item) => (
+        {routes.slice(0, 2).map((item) => (
           <FooterItem
             key={item.label}
             href={item.href}

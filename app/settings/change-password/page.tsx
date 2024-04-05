bfc82c590cd0c1d7dd69e24f0c06dd784d245415
mateuszNadolny <mateusznadolny00@gmail.com>
2024-04-05 14:23:56 +0200

    fix: add dynamic imports to /change-password page dynamic import had to be added to fix the issue coming from Next.js 14 app build

--- app/settings/change-password/page.tsx
+++ app/settings/change-password/page.tsx
@@ -1,5 +1,16 @@
+import dynamic from 'next/dynamic';
+
 import getCurrentUser from '@/actions/getCurrentUser';
-import ChangePasswordForm from '@/components/settings/change-password-form';
+
+// ADDING DYNAMIC IMPORT TO RESOLVE ISSUE COMING FROM APP BUILD (NPM RUN BUILD)
+// https://github.com/vercel/next.js/issues/58576
+// TODO: remove dynamic imports after Next releases fix of the issue
+
+// import ChangePasswordForm from '@/components/settings/change-password-form';
+
+const ChangePasswordForm = dynamic(() => import('@/components/settings/change-password-form'), {
+  ssr: false
+});
 
 const ChangePasswordPage = async () => {
   const user = await getCurrentUser();

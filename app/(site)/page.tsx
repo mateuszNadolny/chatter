477e0dd558dd4a606e4f3e5f9af64e521639a329
mateuszNadolny <mateusznadolny00@gmail.com>
2024-04-25 14:16:38 +0200

    style: fix mobile styling on login page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,9 +3,9 @@ import { ThemeToggle } from '@/components/global/theme-toggle';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-h-screen items-center justify-center">
+    <main className="flex flex-col min-h-screen lg:max-h-screen items-center justify-center pt-10  pb-10 lg:pt-0 lg:pb-0">
       <AuthTabs />
-      <div className="fixed top-5 right-5 lg:top-10 lg:right-10">
+      <div className="absolute top-5 right-5 lg:fixed lg:top-10 lg:right-10">
         <ThemeToggle />
       </div>
     </main>
